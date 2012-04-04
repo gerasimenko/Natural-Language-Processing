e2fa65846760884b98570634bbe238b90ec150ac
Michael <michael.gerasimenko@gmail.com>
2012-04-04 05:22:41 +0300

    homework 2 language models

--- homework2/src/StupidBackoffLanguageModel.java
+++ homework2/src/StupidBackoffLanguageModel.java
@@ -1,12 +1,13 @@
 import sun.text.normalizer.IntTrie;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StupidBackoffLanguageModel implements LanguageModel {
 
-    protected Map<String, Integer> wordsMap;
+    protected Map<MultiGram, Integer> wordsMap;
     protected int k = 2;
     protected int wordsCount = 0;
 
@@ -14,7 +15,7 @@ public class StupidBackoffLanguageModel implements LanguageModel {
      * Initialize your data structures in the constructor.
      */
     public StupidBackoffLanguageModel(HolbrookCorpus corpus) {
-        wordsMap = new HashMap<String, Integer>();
+        wordsMap = new HashMap<MultiGram, Integer>();
 
         train(corpus);
     }
@@ -24,21 +25,30 @@ public class StupidBackoffLanguageModel implements LanguageModel {
      * Compute any counts or other corpus statistics in this function.
      */
     public void train(HolbrookCorpus corpus) {
+
+
         Integer value;
         for (Sentence sentence : corpus.getData()) { // iterate over sentences
             for (int i = 0; i < sentence.size(); i++) { // iterate over words
-                String word = "";
+                MultiGram multiGram = new MultiGram();
                 for(int j = 0; j <= k && (i - j) >= 0; j++) {
-                    String space = (j == 0) ? "" : " ";
-                    word = sentence.get(i - j).getWord() + space + word; // get the actual word
-                    value = wordsMap.get(word);
-
+                    multiGram.data.add(0, sentence.get(i - j).getWord());
+                    value = wordsMap.get(multiGram);
                     if (value == null) {
-                        wordsMap.put(word, 1);
+                        wordsMap.put(multiGram, 1);
                         if (j == 0) wordsCount++;
                     } else {
-                        wordsMap.put(word, value + 1);
+                        wordsMap.put(multiGram, value + 1);
                     }
+                    if (multiGram.data.size() == 2 && multiGram.data.get(0).equals("was") && multiGram.data.get(1).equals("about")) {
+                        MultiGram muligram = new MultiGram();
+                        muligram.data.add(0, "about");
+                        muligram.data.add(0, "was");
+//                        System.out.println(muligram.data);
+
+//                        System.out.println(wordsMap.get(muligram));
+                    }
+
                 }
 
             }
@@ -50,6 +60,10 @@ public class StupidBackoffLanguageModel implements LanguageModel {
      * sentence using your language model. Use whatever data you computed in train() here.
      */
     public double score(List<String> sentence) {
+        MultiGram muligram = new MultiGram();
+        muligram.data.add(0, "about");
+        muligram.data.add(0, "was");
+//        System.out.println(wordsMap.get(muligram));
         double score = 0.0;
         for (int i = k; i < sentence.size(); i++) { // iterate over words in the sentence
             score += Math.log(getScore(sentence, i - k, i));
@@ -80,25 +94,75 @@ public class StupidBackoffLanguageModel implements LanguageModel {
 
     public double getScore(List<String> sentence, int l, int r) {
         if (l < r) {
-            String string1 = "";
-            String string2 = "";
-            for(int i = l; i < r; i++) {
-                String space = (i == l) ? "" : " ";
-                string1 += space + sentence.get(i);
-                string2 += space + sentence.get(i);
+            MultiGram multiGram1 = new MultiGram();
+            MultiGram multiGram2 = new MultiGram();
+
+            multiGram2.data.add(sentence.get(r));
+            for(int i = r - 1; i >= l; i--) {
+                multiGram1.data.add(0, sentence.get(i));
+                multiGram2.data.add(0, sentence.get(i));
             }
-            string2 += " " + sentence.get(r);
-            if (wordsMap.containsKey(string2)) {
-                return wordsMap.get(string2) / (1.0 * wordsMap.get(string1));
+
+//            for(int i = l; i < r; i++) {
+//                multiGram1.data.add(sentence.get(i));
+//                multiGram2.data.add(sentence.get(i));
+//            }
+//            multiGram2.data.add(sentence.get(r));
+            if (wordsMap.containsKey(multiGram2) && wordsMap.containsKey(multiGram1)) {
+//                System.out.print(multiGram2.data + "-" + wordsMap.get(multiGram2));
+//                System.out.println(multiGram1.data + "-" + wordsMap.get(multiGram1));
+                if (multiGram1.data.size() == 2 && multiGram1.data.get(0).equals("was") && multiGram1.data.get(1).equals("about")) {
+                    int tt = multiGram1.hashCode();
+                    tt = tt + 0;
+
+                }
+
+                return wordsMap.get(multiGram2) / (1.0 * wordsMap.get(multiGram1));
+
             } else {
                 return 0.4 * getScore(sentence, l + 1, r);
             }
 
         } else {
-            Integer value = wordsMap.get(sentence.get(l));
+            Integer value = wordsMap.get(new MultiGram(sentence.get(l)));
             value = (value == null) ? 0 : value;
             return (1.0 + value) / (sentence.size() + wordsCount);
         }
 
+    }
+}
+
+class MultiGram {
+    public List<String> data;
+
+    MultiGram() {
+        this.data = new ArrayList<String>();
+    }
+
+    MultiGram(String string) {
+        this();
+        data.add(string);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MultiGram multiGram = (MultiGram) o;
+
+        if (!data.equals(multiGram.data)) return false;
+//        if (data.size() != multiGram.data.size()) return false;
+//        for (int i = 0; i < data.size(); i++) {
+//            if (!data.get(i).equals(multiGram.data.get(i))) return false;
+//
+//        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
     }
 }
