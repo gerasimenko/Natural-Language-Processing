188def9675c1a1432a1acab61799510939154858
Michael <michael.gerasimenko@gmail.com>
2012-04-04 05:51:52 +0300

    homework 2 language models - done!

--- homework2/src/CustomLanguageModel.java
+++ homework2/src/CustomLanguageModel.java
@@ -1,25 +1,84 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomLanguageModel implements LanguageModel {
 
-  /** Initialize your data structures in the constructor. */
-  public CustomLanguageModel(HolbrookCorpus corpus) {
-    train(corpus);
-  }
+    protected Map<String, Integer> wordsMap;
+    protected int k = 3;
+    protected int wordsCount = 0;
 
-  /** Takes a corpus and trains your language model. 
-    * Compute any counts or other corpus statistics in this function.
-    */
-  public void train(HolbrookCorpus corpus) { 
-    // TODO: your code here
-  }
+    /**
+     * Initialize your data structures in the constructor.
+     */
+    public CustomLanguageModel(HolbrookCorpus corpus) {
+        wordsMap = new HashMap<String, Integer>();
 
-  /** Takes a list of strings as argument and returns the log-probability of the 
-    * sentence using your language model. Use whatever data you computed in train() here.
-    */
-  public double score(List<String> sentence) {
-    // TODO: your code here
-    return 0.0;
-  }
+        train(corpus);
+    }
+
+    /**
+     * Takes a corpus and trains your language model.
+     * Compute any counts or other corpus statistics in this function.
+     */
+    public void train(HolbrookCorpus corpus) {
+        Integer value;
+        for (Sentence sentence : corpus.getData()) { // iterate over sentences
+            for (int i = 0; i < sentence.size(); i++) { // iterate over words
+                String word = "";
+                for(int j = 0; j <= k && (i - j) >= 0; j++) {
+                    String space = (j == 0) ? "" : " ";
+                    word = sentence.get(i - j).getWord() + space + word; // get the actual word
+                    value = wordsMap.get(word);
+
+                    if (value == null) {
+                        wordsMap.put(word, 1);
+                        if (j == 0) wordsCount++;
+                    } else {
+                        wordsMap.put(word, value + 1);
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * Takes a list of strings as argument and returns the log-probability of the
+     * sentence using your language model. Use whatever data you computed in train() here.
+     */
+    public double score(List<String> sentence) {
+        double score = 0.0;
+        for (int i = k; i < sentence.size(); i++) { // iterate over words in the sentence
+            score += Math.log(getScore(sentence, i - k, i));
+        }
+        score += Math.log(getScore(sentence, 0, 0));
+        score += Math.log(getScore(sentence, 0, 1));
+        return score;
+    }
+
+    public double getScore(List<String> sentence, int l, int r) {
+        if (l < r) {
+            String string1 = "";
+            String string2 = "";
+            for(int i = l; i < r; i++) {
+                String space = (i == l) ? "" : " ";
+                string1 += space + sentence.get(i);
+                string2 += space + sentence.get(i);
+            }
+            string2 += " " + sentence.get(r);
+            if (wordsMap.containsKey(string2)) {
+                return wordsMap.get(string2) / (1.0 * wordsMap.get(string1));
+            } else {
+                return 0.4 * getScore(sentence, l + 1, r);
+            }
+
+        } else {
+            Integer value = wordsMap.get(sentence.get(l));
+            value = (value == null) ? 0 : value;
+            return (1.0 + value) / (sentence.size() + wordsCount);
+        }
+
+    }
   
 }
--- homework2/src/StupidBackoffLanguageModel.java
+++ homework2/src/StupidBackoffLanguageModel.java
@@ -1,9 +1,6 @@
 import sun.text.normalizer.IntTrie;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StupidBackoffLanguageModel implements LanguageModel {
 
@@ -32,6 +29,7 @@ public class StupidBackoffLanguageModel implements LanguageModel {
             for (int i = 0; i < sentence.size(); i++) { // iterate over words
                 MultiGram multiGram = new MultiGram();
                 for(int j = 0; j <= k && (i - j) >= 0; j++) {
+                    multiGram = multiGram.getClone();
                     multiGram.data.add(0, sentence.get(i - j).getWord());
                     value = wordsMap.get(multiGram);
                     if (value == null) {
@@ -39,14 +37,6 @@ public class StupidBackoffLanguageModel implements LanguageModel {
                         if (j == 0) wordsCount++;
                     } else {
                         wordsMap.put(multiGram, value + 1);
-                    }
-                    if (multiGram.data.size() == 2 && multiGram.data.get(0).equals("was") && multiGram.data.get(1).equals("about")) {
-                        MultiGram muligram = new MultiGram();
-                        muligram.data.add(0, "about");
-                        muligram.data.add(0, "was");
-//                        System.out.println(muligram.data);
-
-//                        System.out.println(wordsMap.get(muligram));
                     }
 
                 }
@@ -67,25 +57,6 @@ public class StupidBackoffLanguageModel implements LanguageModel {
         double score = 0.0;
         for (int i = k; i < sentence.size(); i++) { // iterate over words in the sentence
             score += Math.log(getScore(sentence, i - k, i));
-/*
-            if (wordsMap.containsKey(sentence.get(i - 2) + " " + sentence.get(i - 1) + " " + sentence.get(i))) {
-                score += Math.log(wordsMap.get(sentence.get(i - 2) + " " + sentence.get(i - 1) + " " + sentence.get(i)));
-//                System.out.println(sentence.get(i - 2) + " " + sentence.get(i - 1));
-                score -= Math.log(wordsMap.get(sentence.get(i - 2) + " " + sentence.get(i - 1)));
-            } else {
-                if (wordsMap.containsKey(sentence.get(i - 1) + " " + sentence.get(i))) {
-                    score += Math.log(0.4 * wordsMap.get(sentence.get(i - 1) + " " + sentence.get(i)));
-                    score -= Math.log(wordsMap.get(sentence.get(i - 1)));
-                } else {
-                    if (wordsMap.containsKey(sentence.get(i))) {
-                        score += Math.log(wordsMap.get(sentence.get(i)));
-                        score -= Math.log(wordsCount);
-                    } else {
-                        score += 0;
-                    }
-                }
-            }
-*/
         }
         score += Math.log(getScore(sentence, 0, 0));
         score += Math.log(getScore(sentence, 0, 1));
@@ -97,28 +68,14 @@ public class StupidBackoffLanguageModel implements LanguageModel {
             MultiGram multiGram1 = new MultiGram();
             MultiGram multiGram2 = new MultiGram();
 
-            multiGram2.data.add(sentence.get(r));
-            for(int i = r - 1; i >= l; i--) {
-                multiGram1.data.add(0, sentence.get(i));
-                multiGram2.data.add(0, sentence.get(i));
+
+            for(int i = l; i < r; i++) {
+                multiGram1.data.add(sentence.get(i));
+                multiGram2.data.add(sentence.get(i));
             }
-
-//            for(int i = l; i < r; i++) {
-//                multiGram1.data.add(sentence.get(i));
-//                multiGram2.data.add(sentence.get(i));
-//            }
-//            multiGram2.data.add(sentence.get(r));
+            multiGram2.data.add(sentence.get(r));
             if (wordsMap.containsKey(multiGram2) && wordsMap.containsKey(multiGram1)) {
-//                System.out.print(multiGram2.data + "-" + wordsMap.get(multiGram2));
-//                System.out.println(multiGram1.data + "-" + wordsMap.get(multiGram1));
-                if (multiGram1.data.size() == 2 && multiGram1.data.get(0).equals("was") && multiGram1.data.get(1).equals("about")) {
-                    int tt = multiGram1.hashCode();
-                    tt = tt + 0;
-
-                }
-
                 return wordsMap.get(multiGram2) / (1.0 * wordsMap.get(multiGram1));
-
             } else {
                 return 0.4 * getScore(sentence, l + 1, r);
             }
@@ -152,17 +109,19 @@ class MultiGram {
         MultiGram multiGram = (MultiGram) o;
 
         if (!data.equals(multiGram.data)) return false;
-//        if (data.size() != multiGram.data.size()) return false;
-//        for (int i = 0; i < data.size(); i++) {
-//            if (!data.get(i).equals(multiGram.data.get(i))) return false;
-//
-//        }
-
         return true;
     }
 
     @Override
     public int hashCode() {
         return data.hashCode();
+    }
+
+    MultiGram getClone() {
+        MultiGram clone = new MultiGram();
+        for(String string : data) {
+            clone.data.add(new String(string));
+        }
+        return clone;
     }
 }
